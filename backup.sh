97455f77e9b881fdb35f374e7720e932f55d6dc5
Gordon Sun <sunshineo@gmail.com>
2016-10-02 01:15:22 -0700

    Better logs. Easier to figure out what is going on.

--- backup.sh
+++ backup.sh
@@ -3,18 +3,25 @@ export PATH=$PATH:/usr/bin:/usr/local/bin:/bin
 # Get timestamp
 : ${BACKUP_SUFFIX:=.$(date +"%Y-%m-%d-%H-%M-%S")}
 readonly tarball=$BACKUP_NAME$BACKUP_SUFFIX.tar.gz
+echo "Going to create tarball: $tarball for path: $PATHS_TO_BACKUP with options $BACKUP_TAR_OPTION"
 
 # Create a gzip compressed tarball with the volume(s)
 tar czf $tarball $BACKUP_TAR_OPTION $PATHS_TO_BACKUP
 
 # Create bucket, if it doesn't already exist
+echo "Command: aws s3 ls | grep $S3_BUCKET_NAME | wc -l"
+aws s3 ls | grep $S3_BUCKET_NAME | wc -l
 BUCKET_EXIST=$(aws s3 ls | grep $S3_BUCKET_NAME | wc -l)
 if [ $BUCKET_EXIST -eq 0 ];
+	echo "Bucket already exists."
 then
+  echo "Bucket does not exist. Create now."
+  echo "Command: aws s3 mb s3://$S3_BUCKET_NAME"
   aws s3 mb s3://$S3_BUCKET_NAME
 fi
 
 # Upload the backup to S3 with timestamp
+echo "Command: aws s3 --region $AWS_DEFAULT_REGION cp $tarball s3://$S3_BUCKET_NAME/$tarball"
 aws s3 --region $AWS_DEFAULT_REGION cp $tarball s3://$S3_BUCKET_NAME/$tarball
 
 # Clean up
--- restore.sh
+++ restore.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
 # Find last backup file
+echo "Command: aws s3 ls s3://$S3_BUCKET_NAME"
+aws s3 ls s3://$S3_BUCKET_NAME
+echo "Command: aws s3 ls s3://$S3_BUCKET_NAME | awk -F \" \" '{print $4}'"
+aws s3 ls s3://$S3_BUCKET_NAME | awk -F " " '{print $4}'
+echo "Command: aws s3 ls s3://$S3_BUCKET_NAME | awk -F \" \" '{print $4}' | grep ^$BACKUP_NAME"
+aws s3 ls s3://$S3_BUCKET_NAME | awk -F " " '{print $4}' | grep ^$BACKUP_NAME
+echo "Command: aws s3 ls s3://$S3_BUCKET_NAME | awk -F \" \" '{print $4}' | grep ^$BACKUP_NAME | sort -r"
+aws s3 ls s3://$S3_BUCKET_NAME | awk -F " " '{print $4}' | grep ^$BACKUP_NAME | sort -r
+echo "Command: aws s3 ls s3://$S3_BUCKET_NAME | awk -F \" \" '{print $4}' | grep ^$BACKUP_NAME | sort -r | head -n1"
+aws s3 ls s3://$S3_BUCKET_NAME | awk -F " " '{print $4}' | grep ^$BACKUP_NAME | sort -r | head -n1
+
 : ${LAST_BACKUP:=$(aws s3 ls s3://$S3_BUCKET_NAME | awk -F " " '{print $4}' | grep ^$BACKUP_NAME | sort -r | head -n1)}
+echo "We think the last backup is:"
+echo $LAST_BACKUP
 
 # Download backup from S3
+echo "Command: aws s3 cp s3://$S3_BUCKET_NAME/$LAST_BACKUP $LAST_BACKUP"
 aws s3 cp s3://$S3_BUCKET_NAME/$LAST_BACKUP $LAST_BACKUP
 
 # Extract backup
+echo "Command: tar xzf $LAST_BACKUP $RESTORE_TAR_OPTION"
 tar xzf $LAST_BACKUP $RESTORE_TAR_OPTION
